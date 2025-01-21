a1dd40dd13a983158d6f01dc1581ceae52106905
Agrima440 <agrimasharma47@gmail.com>
2025-01-21 22:07:23 +0530

    first commit

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.get("/", (req, res) =>
 );
 app.use("/api/email", emailRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
